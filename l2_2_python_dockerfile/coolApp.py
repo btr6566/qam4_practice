0ec5b0d1769baf2c4132a69e26e782efc371bd56
btroberts6566 <ben.roberts6566@gmail.com>
2024-09-12 11:36:53 +0100

    Update coolApp.py
    required:
    - rebuild
    - retag
    - pushing new version

--- l2_2_python_dockerfile/coolApp.py
+++ l2_2_python_dockerfile/coolApp.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/home')
 @app.route('/')
 def home():
-    return "Hello world!"
+    return "<h1>Hello world!</h1>"
 
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0', debug=True)
